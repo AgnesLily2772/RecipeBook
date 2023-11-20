0927366635d8767b959d37ae07207e189cfc5e88
AGNES LILY <97900099+AgnesLily2772@users.noreply.github.com>
2023-11-20 12:33:21 +0530

    Cleanup
    In this commit, I have cleaned up code. Added an error route that redirects all the unknown routes towards signin page.

--- frontend/src/Utils/globals.js
+++ frontend/src/Utils/globals.js
@@ -1,2 +1,5 @@
-// export const SERVER_URL = "https://recipebook-backend-rmgn.onrender.com/api"
-export const SERVER_URL = "http://localhost:5000/api"
+//production
+export const SERVER_URL = "https://recipebook-backend-rmgn.onrender.com/api"
+
+//devlopment
+// export const SERVER_URL = "http://localhost:5000/api"
